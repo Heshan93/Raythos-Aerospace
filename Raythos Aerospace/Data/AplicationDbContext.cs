2818506608442a5932bf92d3452521412bce8aa3
Heshan93 <heshanrajitha1@gmail.com>
2023-12-24 13:15:39 +0530

    Add project files.

---
+++ Raythos Aerospace/Data/AplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Raythos_Aerospace.Models;
+
+namespace Raythos_Aerospace.Data
+{
+    public class AplicationDbContext :DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Catagory> Catagorys { get; set; }
+    }
+}
