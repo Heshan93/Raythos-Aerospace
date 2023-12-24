1fe0b62097f1b4c1b3206d3e7f04e66c67db37d4
Heshan93 <heshanrajitha1@gmail.com>
2023-12-24 18:30:31 +0530

    Sample View / Create Functions with view

---
+++ Raythos Aerospace/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Raythos_Aerospace.Data;
+using Raythos_Aerospace.Models;
+
+namespace Raythos_Aerospace.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AplicationDbContext _db;
+
+        public CategoryController(AplicationDbContext db)
+        {
+                _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Catagory> objectCategoryList = _db.Catagorys ;
+
+            return View(objectCategoryList);
+        }
+        //Get
+        public IActionResult Create()
+        {
+     
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Catagory obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "DisplayOrder can not be same as the name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Catagorys.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+    }
+}
